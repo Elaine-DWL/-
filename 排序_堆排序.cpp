f234a9ab4cf92c4b6e213f925621b11129ac72cc
Elaine-DWL <1120164345@qq.com>
2019-07-06 10:23:27 +0800

    修改堆排序

--- 排序_堆排序.cpp
+++ 排序_堆排序.cpp
@@ -1,55 +1,23 @@
-class Solution{
+class Solution {
 public:
-		void heap_sort(vector<int> &nums){
-			// 大顶堆的构建
-			int n = nums.size()-1;
-			int l,r,f;
-			int idx_max;
-			for(int i=n; i>0;){// 构建大顶堆 从下往上
-				if(i%2==0 && i!=0){// i是偶数 右儿子结点
-					l = nums[i-1];
-					r = nums[i];
-					f = nums[(i-1)/2];
-					idx_max = l>r?i-1:i;
-					if(nums[idx_max] > f) swap(nums[idx_max], nums[(i-1)/2]);
-					i = i-2;
-				}
-				else if(i%2==1){
-					l = nums[i];
-					f = nums[i/2];
-					if(l>f) swap(nums[i], nums[i/2]);
-					i--;
-				}
-			}
-			cout<<"初始化大顶堆结果："<<endl;
-			for(int num:nums)
-				cout<<num<<" ";
-			cout<<endl;
-			
-			for(int i=n; i>0; i--){// 确定nums[i]处的元素
-				swap(nums[i], nums[0]);
-				//大顶堆的调整 从上往下调整
-				for(int j=0; 2*j+1<i;){
-					l = nums[2*j+1];
-					f = nums[j];
-					if(2*j+2==i && l>f){
-						swap(nums[j], nums[2*j+1]);
-						j = 2*j+1;
-					} 
-					else if(2*j+2<i){
-						r =  nums[2*j+2];
-						idx_max = l>r?2*j+1:2*j+2;
-						if(nums[idx_max] > f){
-							swap(nums[idx_max], nums[j]);
-							j = idx_max;
-						}
-						else break;
-					}
-					else break;
-				}
-			}
-			cout<<"堆排序结果："<<endl;
-			for(int num:nums) cout<<num<<" ";
-			return;
-		}
+    void adjust(vector<int> &nums, int pos, int tail){// 对pos及其子树 进行调整  从上往下调整
+        int idx = pos;
+        if(2*pos+1 <= tail && nums[2*pos+1]>nums[idx]) idx = 2*pos+1;
+        if(2*pos+2 <= tail && nums[2*pos+2]>nums[idx]) idx = 2*pos+2;
+        if(idx != pos){
+            swap(nums[idx], nums[pos]);
+            adjust(nums, idx, tail);// 递归的方法，如果进行了交换 对叶子结点进行调整
+        }
 }
+    vector<int> sortArray(vector<int>& nums) {
+        // 初始化大顶堆
+        int n = nums.size();
+        for(int i=(n-1)/2; i>=0; i--) adjust(nums, i, n-1); // 从第一个非叶子结点 从下往上调整
+        // 对大顶堆进行排序
+        for(int i=n-1; i>=1; i--){
+            swap(nums[0], nums[i]);
+            adjust(nums, 0, i-1);
+        }
+        return nums; // 现在返回的数组就是 从小到大排序的
+    }
+};

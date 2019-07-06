a4192573ee6cfb6efc05330b9bd615c77e299ac8
Elaine-DWL <1120164345@qq.com>
2019-07-06 10:39:17 +0800

    修改 快速排序

--- 排序_快速排序.cpp
+++ 排序_快速排序.cpp
@@ -3,29 +3,29 @@
 // 将大于该基准点的元素都放到其右边
 // 然后将剩下的两个子区间(左边和右边的)都按上述方法进行排序
 
-// 下面代码在leetcode215上测试了
+// 下面代码在leetcode912上测试了
 class Solution {
 public:
-    int partition(vector<int> &nums, int p, int q){//以nums[q]为标准   将小于它的放左边  大于它的放右边
+    int partition(vector<int> &nums, int p, int q){
+        // 确定 p正确位置 并返回
         while(p<q){
+            while(p<q && nums[q] >= nums[p]) q--;
+            swap(nums[p], nums[q]);
             while(p<q && nums[p] <= nums[q]) p++;
             swap(nums[p], nums[q]);
-            while(p<q && nums[q] >= nums[p]) q--;
-            swap(nums[p], nums[q]);   
         }
-        return p;// 返回已经确定的nums[q]的下标  因为上面while循环结束时p==q 这里返回p,q都可以 
+        return p;
     }
-    int quick_sort(vector<int> &nums, int p, int q){
-        if(p>=q) return 0;// 这里的返回值没有意义  我只是想让递归终止
-        int ipovt = partition(nums, p, q);
-        quick_sort(nums, p, ipovt-1);
-        quick_sort(nums, ipovt+1,  q);
-        return 1;
+    void quick_sort(vector<int> &nums, int p, int q){
+        if(p>=q) return;
+        int pivot = partition(nums, p, q);
+        quick_sort(nums, p, pivot-1);
+        quick_sort(nums, pivot+1, q);
+        
     }
-    int findKthLargest(vector<int>& nums, int k) {
+    vector<int> sortArray(vector<int>& nums) {
+        // 快速排序  从小到大
         quick_sort(nums, 0, nums.size()-1);
-        for(int x:nums) cout<<x<<" ";
-        cout<<endl;
-        return -1;
+        return nums;
     }
 };
